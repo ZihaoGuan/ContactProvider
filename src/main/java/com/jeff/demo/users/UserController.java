7b7f23b9906228b3574525bc0bfa8d44404b522b
ZihaoGuan <grand_zh@yeah.net>
2022-02-03 00:38:18 +1300

    added modelmapper dependency, added UserDto class

--- src/main/java/com/jeff/demo/users/UserController.java
+++ src/main/java/com/jeff/demo/users/UserController.java
@@ -1,7 +1,8 @@
 package com.jeff.demo.users;
 
-import com.jeff.demo.users.model.User;
+import com.jeff.demo.users.dto.UserDto;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,35 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+	private ModelMapper modelMapper;
+    @Autowired
     private UserService userService;
 
     @GetMapping("/getusercontacts")
-    public User getUserContacts(@RequestParam(value = "username", required = false) String name,
+    public UserDto getUserContacts(@RequestParam(value = "username", required = false) String name,
             @RequestParam(value = "id", required = false) Long id) {
 
         //if both parameters not exist, return user with id=-1 
         if (id == null && name == null) {
-            User temp = new User();
+            UserDto temp = new UserDto();
             temp.setId(-1L);
             return temp;
         }
 
         //if both parameters are provided, return user with id=-1 
         if (id != null && name != null) {
-            User temp = new User();
+            UserDto temp = new UserDto();
             temp.setId(-1L);
             return temp;
         }
 
         if (id != null) {
-            return userService.getUserContactsById(id);
+            return modelMapper.map(userService.getUserContactsById(id), UserDto.class);
         }
 
         if (name != null) {
-            return userService.getUserContactsByUsername(name);
+            return modelMapper.map(userService.getUserContactsByUsername(name), UserDto.class);
         }
 
-        User temp = new User();
+        UserDto temp = new UserDto();
         temp.setId(-1L);
         return temp;
     }
---
+++ src/main/java/com/jeff/demo/users/dto/UserDto.java
@@ -0,0 +1,41 @@
+package com.jeff.demo.users.dto;
+
+public class UserDto {
+    private Long id;
+    private String email;
+    private String phone;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", phone=" + phone + '\'' +
+                '}';
+    }
+
+}
