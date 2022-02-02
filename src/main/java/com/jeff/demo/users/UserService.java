1daeb581184b04a38ddb92883df3fd14a6388484
ZihaoGuan <grand_zh@yeah.net>
2022-02-03 08:49:43 +1300

    Added logger

--- src/main/java/com/jeff/demo/users/UserService.java
+++ src/main/java/com/jeff/demo/users/UserService.java
@@ -6,11 +6,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jeff.demo.users.model.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public User getUserContactsById(Long id) {
         User[] filteredUsers = Arrays.stream(fetchUserContacts()).filter(u -> u.getId() == id).toArray(User[]::new);
@@ -25,12 +29,14 @@ public class UserService {
     private User[] fetchUserContacts(){
         User [] users = {};
 
+        logger.info("Fetching data from third party API.");
         String uri = "https://jsonplaceholder.typicode.com/users";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         ObjectMapper mapper = new ObjectMapper();
         try {
             users = mapper.readValue(result, User[].class);
+            logger.info("Data fetched successfully.");
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
