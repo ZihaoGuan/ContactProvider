831a76171325b19e7e924711c0f3c826d3cf339d
ZihaoGuan <grand_zh@yeah.net>
2022-02-03 08:22:59 +1300

    refactor user service

--- src/main/java/com/jeff/demo/users/UserService.java
+++ src/main/java/com/jeff/demo/users/UserService.java
@@ -12,45 +12,34 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class UserService {
 
-    private User[] users;
+    public User getUserContactsById(Long id) {
+        User[] filteredUsers = Arrays.stream(fetchUserContacts()).filter(u -> u.getId() == id).toArray(User[]::new);
+        return filteredUsers.length == 0 ? userNotFound() : filteredUsers[0];
+    }
 
-    // read and store data from thrid party API.
-    public UserService() {
+    public User getUserContactsByUsername(String username) {
+        User[] filteredUsers = Arrays.stream(fetchUserContacts()).filter(u -> u.getUsername().equals(username)).toArray(User[]::new);
+        return filteredUsers.length == 0 ? userNotFound() : filteredUsers[0];
+    }
 
-        final String uri = "https://jsonplaceholder.typicode.com/users";
+    private User[] fetchUserContacts(){
+        User [] users = {};
 
+        String uri = "https://jsonplaceholder.typicode.com/users";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
-
         ObjectMapper mapper = new ObjectMapper();
         try {
             users = mapper.readValue(result, User[].class);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
+        return users;
     }
 
-    public User getUserContactsById(Long id) {
-        User[] filteredUsers = Arrays.stream(users).filter(u -> u.getId() == id).toArray(User[]::new);
-
-        if (filteredUsers.length == 0) {
-            User temp = new User();
-            temp.setId(-1L);
-            return temp;
-        }
-
-        return filteredUsers[0];
-    }
-
-    public User getUserContactsByUsername(String username) {
-        User[] filteredUsers = Arrays.stream(users).filter(u -> u.getUsername().equals(username)).toArray(User[]::new);
-
-        if (filteredUsers.length == 0) {
-            User temp = new User();
-            temp.setId(-1L);
-            return temp;
-        }
-
-        return filteredUsers[0];
+    private User userNotFound() {
+        User temp = new User();
+        temp.setId(-1L);
+        return temp;
     }
 }
