4cd6bb020a8a04376417d94d14d0da82d3bf49fd
ZihaoGuan <grand_zh@yeah.net>
2022-02-03 10:44:39 +1300

    fix user controller to deal with invalid id

--- src/main/java/com/jeff/demo/users/UserController.java
+++ src/main/java/com/jeff/demo/users/UserController.java
@@ -2,14 +2,18 @@ package com.jeff.demo.users;
 
 import com.jeff.demo.users.dto.UserDto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UserController {
+    Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private ModelMapper modelMapper;
@@ -18,7 +22,7 @@ public class UserController {
 
     @GetMapping("/getusercontacts")
     public UserDto getUserContacts(@RequestParam(value = "username", required = false) String name,
-            @RequestParam(value = "id", required = false) Long id) {
+            @RequestParam(value = "id", required = false) String id) {
 
         // if both parameters not exist, return user with id=-1
         if (id == null && name == null)
@@ -28,8 +32,14 @@ public class UserController {
         if (id != null && name != null)
             return userNotFound();
 
-        if (id != null)
-            return modelMapper.map(userService.getUserContactsById(id), UserDto.class);
+        if (id != null){
+            try {
+                return modelMapper.map(userService.getUserContactsById(Long.parseLong(id)), UserDto.class);
+            } catch (java.lang.NumberFormatException e){
+                logger.error("Request with invalid ID");
+                return userNotFound();
+            }
+        }  
 
         if (name != null)
             return modelMapper.map(userService.getUserContactsByUsername(name), UserDto.class);
