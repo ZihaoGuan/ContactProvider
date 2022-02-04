9d03a170a01fae724b9f622fd263b0c07ba8b396
ZihaoGuan <grand_zh@yeah.net>
2022-02-05 00:42:14 +1300

    fix test naming

--- src/test/java/com/jeff/demo/UserControllerTest.java
+++ src/test/java/com/jeff/demo/UserControllerTest.java
@@ -16,21 +16,21 @@ class UserControllerTest {
 	MockMvc mockMvc;
 
 	@Test
-	void getContacsWithoutParameterShouldGetUserNotFound() throws Exception {
+	void getContactsWithoutParameterShouldGetUserNotFound() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts"))
 		.andExpect(content().json("{'id':-1,'email':null,'phone':null}"));
 	}
 
 	@Test
-	void getContacsWithBothParameters() throws Exception {
+	void getContactsWithBothParameters() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?id=1&username=Bret"))
 		.andExpect(content().json("{'id':1,'email':'Sincere@april.biz','phone':'1-770-736-8031 x56442'}'"));
 	}
 
 	@Test
-	void getContacsWithBothParametersButNotMatch() throws Exception {
+	void getContactsWithBothParametersButNotMatch() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?id=1&username=Brett"))
 		.andExpect(content().json("{'id':-1,'email':null,'phone':null}"));
@@ -38,35 +38,35 @@ class UserControllerTest {
 
 
 	@Test
-	void getContacsByValidId() throws Exception {
+	void getContactsByValidId() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?id=1"))
 		.andExpect(content().json("{'id':1,'email':'Sincere@april.biz','phone':'1-770-736-8031 x56442'}'"));
 	}
 
 	@Test
-	void getContacsByInValidId() throws Exception {
+	void getContactsByInValidId() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?id=x"))
 		.andExpect(content().json("{'id':-1,'email':null,'phone':null}"));
 	}
 
 	@Test
-	void getContacsByIdNotExist() throws Exception {
+	void getContactsByIdNotExist() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?id=999"))
 		.andExpect(content().json("{'id':-1,'email':null,'phone':null}"));
 	}
 
 	@Test
-	void getContacsByValidUsername() throws Exception {
+	void getContactsByValidUsername() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?username=Bret"))
 		.andExpect(content().json("{'id':1,'email':'Sincere@april.biz','phone':'1-770-736-8031 x56442'}'"));
 	}
 
 	@Test
-	void getContacsByUsernameNotExist() throws Exception {
+	void getContactsByUsernameNotExist() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 		.get("/getusercontacts?username=Brett"))
 		.andExpect(content().json("{'id':-1,'email':null,'phone':null}"));
