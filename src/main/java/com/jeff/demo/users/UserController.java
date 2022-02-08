dd9b753fd95751437aa20248d6a145545e16472f
ZihaoGuan <grand_zh@yeah.net>
2022-02-09 10:17:10 +1300

    refactored user controller and service

--- src/main/java/com/jeff/demo/users/UserController.java
+++ src/main/java/com/jeff/demo/users/UserController.java
@@ -2,8 +2,6 @@ package com.jeff.demo.users;
 
 import com.jeff.demo.users.dto.UserDto;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -20,33 +17,7 @@ public class UserController {
     public UserDto getUserContacts(@RequestParam(value = "username", required = false) String username,
             @RequestParam(value = "id", required = false) String id) {
 
-        UserDto result = userNotFound();
-        
-        if (id != null && username != null) {
-            try {
-                UserDto result1 = new UserDto(userService.getUserContactsById(Long.parseLong(id)));
-                UserDto result2 = new UserDto(userService.getUserContactsByUsername(username));
-
-                result = result1.equals(result2) ? result1 : userNotFound();
-            } catch (java.lang.NumberFormatException e) {
-                logger.error("Request with invalid ID");
-            }
-        } else if (id != null) {
-            try {
-                result = new UserDto(userService.getUserContactsById(Long.parseLong(id)));
-            } catch (java.lang.NumberFormatException e) {
-                logger.error("Request with invalid ID");
-            }
-        } else if (username != null)
-            result = new UserDto(userService.getUserContactsByUsername(username));
-
-        return result;
-    }
-
-    private UserDto userNotFound() {
-        UserDto temp = new UserDto();
-        temp.setId(-1L);
-        return temp;
+        return userService.getUserContacts(id, username);
     }
 
 }
--- src/main/java/com/jeff/demo/users/UserService.java
+++ src/main/java/com/jeff/demo/users/UserService.java
@@ -2,6 +2,7 @@ package com.jeff.demo.users;
 
 import java.util.Arrays;
 
+import com.jeff.demo.users.dto.UserDto;
 import com.jeff.demo.users.model.User;
 
 import org.slf4j.Logger;
@@ -19,12 +20,37 @@ public class UserService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public User getUserContactsById(Long id) {
+    public UserDto getUserContacts(String idString, String username){
+
+        UserDto result = new UserDto(userNotFound());
+
+        if (idString != null && username != null) {
+            try {
+                UserDto result1 = new UserDto(getUserContactsById(Long.parseLong(idString)));
+                UserDto result2 = new UserDto(getUserContactsByUsername(username));
+
+                result = result1.equals(result2) ? result1 : result;
+            } catch (java.lang.NumberFormatException e) {
+                logger.error("Request with invalid ID");
+            }
+        } else if (idString != null) {
+            try {
+                result = new UserDto(getUserContactsById(Long.parseLong(idString)));
+            } catch (java.lang.NumberFormatException e) {
+                logger.error("Request with invalid ID");
+            }
+        } else if (username != null)
+            result = new UserDto(getUserContactsByUsername(username));
+
+        return result;
+    }
+
+    private User getUserContactsById(Long id) {
         User[] filteredUsers = Arrays.stream(fetchUserContacts()).filter(u -> u.getId() == id).toArray(User[]::new);
         return filteredUsers.length == 0 ? userNotFound() : filteredUsers[0];
     }
 
-    public User getUserContactsByUsername(String username) {
+    private User getUserContactsByUsername(String username) {
         User[] filteredUsers = Arrays.stream(fetchUserContacts()).filter(u -> u.getUsername().equals(username)).toArray(User[]::new);
         return filteredUsers.length == 0 ? userNotFound() : filteredUsers[0];
     }
